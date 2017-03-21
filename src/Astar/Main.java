8d5a305b5e390e67b544d26e12aaf333bb093cca
Jean Antunes <jean.antunes@shareprime.com.br>
2017-03-21 17:31:11 -0300

    Astar

---
+++ src/Astar/Main.java
@@ -0,0 +1,40 @@
+package Astar;
+
+import Kalman.*;
+import lejos.nxt.*;
+
+public class Main {
+	public static void main(String[] args) throws InterruptedException{
+
+		//Kalman kalman = new Kalman(0.7782, 0.3996, 50);
+		
+		
+		//tamanho robo = 23x23
+		//turn(2880, 1); 
+		
+    	
+		
+    	turn(3050, 2); //esquerda
+	
+
+		Button.waitForAnyPress();
+	}
+	public static void turn(int degrees, int direction)  throws InterruptedException{
+		
+    	Motor.B.setSpeed(200);
+    	Motor.C.setSpeed(200);
+
+    	if(direction == 1) { // direita
+    		Motor.B.rotate( degrees, true);
+    		Motor.C.rotate( -degrees, true);
+    	}
+    	else  {
+    		Motor.B.rotate( -degrees, true);
+    		Motor.C.rotate( degrees, true);
+    	}
+    	
+    	Thread.sleep(10000);
+		Motor.B.stop();
+		Motor.C.stop();
+	}
+}
